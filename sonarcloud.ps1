ef50862f32bbe33c8075b6e0ed16aff5fd6fa84d
Kamil <kamil.bugno@onet.pl>
2021-09-16 20:55:41 +0200

    add test coverage

---
+++ sonarcloud.ps1
@@ -0,0 +1,33 @@
+param(
+    [string] $sonarSecret
+)
+
+
+Install-package BuildUtils -Confirm:$false -Scope CurrentUser -Force
+Import-Module BuildUtils
+
+$runningDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+$testOutputDir = "$runningDirectory/TestResults"
+
+if (Test-Path $testOutputDir) 
+{
+    Write-host "Cleaning temporary Test Output path $testOutputDir"
+    Remove-Item $testOutputDir -Recurse -Force
+}
+
+
+$version = Invoke-Gitversion
+$assemblyVer = $version.assemblyVersion 
+
+$branch = git branch --show-current
+Write-Host "branch is $branch"
+
+dotnet tool run dotnet-sonarscanner begin /k:"KamilBugnoKrk_WordTester" /v:"$assemblyVer" /o:"kamilbugnokrk" /d:sonar.login="$sonarSecret" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.branch.name="$branch"
+
+dotnet restore
+dotnet build --no-restore
+dotnet test "MyBlazorApp.Tests\MyBlazorApp.Tests.csproj" --collect:"XPlat Code Coverage" --results-directory TestResults/ --logger "trx;LogFileName=unittests.trx" --no-build --no-restore --configuration release -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
+         
+
+dotnet tool run dotnet-sonarscanner end /d:sonar.login="$sonarSecret"
