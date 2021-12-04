98ba0ed9085bdec50e3a99556f589ea4eccb687a
Kamil <kamil.bugno@onet.pl>
2021-12-04 21:17:46 +0100

    fix unit test

--- MyBlazorApp.Tests/LearningAlgorithm/RepetitionManagerTests.cs
+++ MyBlazorApp.Tests/LearningAlgorithm/RepetitionManagerTests.cs
@@ -37,7 +37,7 @@ namespace MyBlazorApp.Tests.LearningAlgorithm
                     TranslatedWord = "TranslatedWord",
                     OriginalWord = "OriginalWord"
                 },
-                UsedRepetitionTypes = "[1,3,4,5,6,7,8,9,10,11]"
+                UsedRepetitionTypes = "[1,3,4,5,6,7,8,9,10,11,12,13]"
             });
             
             question.Should().Be("TranslatedWord");
